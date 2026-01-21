10b57c6ed6e029a7d0803e6e6c76f6442faa653d
PengenNgoding <princelukas22@gmail.com>
2026-01-21 11:15:22 +0700

    fix: move seeds to database/seeders

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -9,12 +11,12 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'user_id'    => '1',
-            'password'   => Hash::make('1234'),
-            'nama'       => 'lukas',
-            'alamat'     => 'panter',
-            'kota'       => 'jakarta',
-            'user_type'  => 'admin',
+            'user_id' => '1',
+            'password' => Hash::make('1234'),
+            'nama' => 'lukas',
+            'alamat' => 'panter',
+            'kota' => 'jakarta',
+            'user_type' => 'admin',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
